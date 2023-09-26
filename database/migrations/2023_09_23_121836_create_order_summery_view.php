8ca7b746b61c39369578a720b0f22dd93e89fc7b
Sohag Hasan <sohag.web.dev@gmail.com>
2023-09-27 00:12:57 +0600

    Error fix for week day start

--- database/migrations/2023_09_23_121836_create_order_summery_view.php
+++ database/migrations/2023_09_23_121836_create_order_summery_view.php
@@ -27,7 +27,7 @@ return new class extends Migration
             JOIN payments p ON p.id = o.payment_id
             JOIN users u ON u.id = o.user_id
             WHERE YEAR(o.created_at) = YEAR(CURRENT_DATE)
-            AND WEEK(o.created_at, 1) = WEEK(CURRENT_DATE)
+            AND WEEK(o.created_at, 1) = WEEK(CURRENT_DATE, 1)
             GROUP BY o.id
             ORDER BY o.id DESC
         ');
